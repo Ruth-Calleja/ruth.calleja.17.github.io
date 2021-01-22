d302363d852b041f32f2b010b6b163cd2eacea17
Ruth-Calleja <61376068+Ruth-Calleja@users.noreply.github.com>
2021-01-22 15:05:52 +0100

    Final Assignment Commit
    Made the form reset after the user clicks submit and triggers toast

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-
+// Form triggers a toast and then resets
 document.getElementById("form").addEventListener("submit", function(e){
 
   e.preventDefault()
+  document.getElementById("form").reset()
   var toastElList = [].slice.call(document.querySelectorAll('.toast'))
   var toastList = toastElList.map(function(toastEl) {
     return new bootstrap.Toast(toastEl)
